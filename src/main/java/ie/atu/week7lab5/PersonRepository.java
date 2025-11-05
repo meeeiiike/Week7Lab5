eb005823fc4bde17ebad4501ee8ee01830cdff27
G00407095@atu.ie <G00407095@atu.ie>
2025-11-05 15:00:12 +0000

    created PersonRepository which extends JpaRepository allowing us to use a load of methods such as findBy, here we have a custom method called findByEmployeeId.

---
+++ src/main/java/ie/atu/week7lab5/PersonRepository.java
@@ -0,0 +1,9 @@
+package ie.atu.week7lab5;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmployeeId(String employeeId);
+}
